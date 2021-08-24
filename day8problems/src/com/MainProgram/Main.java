d1656e8b9b9e10e17e719c796665a3ee1bd51049
jayesh200026 <jayeshkumar8660@gmail.com>
2021-08-24 22:18:34 +0530

    updated Main funtion

--- day8problems/src/com/MainProgram/Main.java
+++ day8problems/src/com/MainProgram/Main.java
@@ -1,9 +1,31 @@
 package com.MainProgram;
 
+import com.CoreProgram.FlipCoin;
+import com.CoreProgram.HarmonicNumber;
+import com.CoreProgram.LargestOf3Numbers;
+import com.CoreProgram.LeapYear;
+import com.CoreProgram.OddOrEven;
+import com.CoreProgram.PowerOf2;
+import com.CoreProgram.PrimeFactors;
+import com.CoreProgram.QuotientAndRemainder;
+import com.CoreProgram.Swap2Numbers;
+import com.CoreProgram.VowelOrConsonant;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("welcome to day8 assignment");
+		
+		//FlipCoin.flipcoin();
+		//LeapYear.leapYear();
+		//PowerOf2.powerOf2(Integer.parseInt(args[0]));
+		//HarmonicNumber.harmonicNumber();
+		//PrimeFactors.primeFactors();
+		//QuotientAndRemainder.quotientAndRemainder();
+		//Swap2Numbers .swap2Numbers();
+		//OddOrEven.oddOreven();
+		//VowelOrConsonant.vowelOrConsonant();
+		//LargestOf3Numbers.largestOf3Numbers();
 
 	}
 
